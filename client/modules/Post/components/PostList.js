41853819e6bd519052295a7edc35adee61ad08e3
Joanna Krezolek <j.krezolek@gmail.com>
2018-05-01 10:12:57 +0200

    working on issue

---
+++ client/modules/Post/components/PostList.js
@@ -0,0 +1,36 @@
+import React, { PropTypes } from 'react';
+
+// Import Components
+import PostListItem from './PostListItem/PostListItem';
+
+function PostList(props) {
+  return (
+    <div className="listView">
+      {
+        props.posts.map(post => (
+          <PostListItem
+            post={post}
+            key={post.cuid}
+            onDelete={() => props.handleDeletePost(post.cuid)}
+            thumbUp={() => props.handleThumbUp(post.cuid)}
+          />
+        ))
+      }
+    </div>
+  );
+}
+
+PostList.propTypes = {
+  posts: PropTypes.arrayOf(PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    content: PropTypes.string.isRequired,
+    votes: PropTypes.number.isRequired,
+    slug: PropTypes.string.isRequired,
+    cuid: PropTypes.string.isRequired,
+  })).isRequired,
+  handleDeletePost: PropTypes.func.isRequired,
+  handleThumbUp: PropTypes.func.isRequired
+};
+
+export default PostList;
---
+++ client/modules/Post/pages/PostListPage/PostListPage.js
@@ -0,0 +1,72 @@
+import React, { PropTypes, Component } from 'react';
+import { connect } from 'react-redux';
+
+// Import Components
+import PostList from '../../components/PostList';
+import PostCreateWidget from '../../components/PostCreateWidget/PostCreateWidget';
+
+// Import Actions
+import { addPostRequest, fetchPosts, deletePostRequest, thumbUpPost, thumbUpPostRequest } from '../../PostActions';
+import { toggleAddPost } from '../../../App/AppActions';
+
+// Import Selectors
+import { getShowAddPost } from '../../../App/AppReducer';
+import { getPosts } from '../../PostReducer';
+
+class PostListPage extends Component {
+  componentDidMount() {
+    this.props.dispatch(fetchPosts());
+  }
+
+  handleDeletePost = post => {
+    if (confirm('Do you want to delete this post')) { // eslint-disable-line
+      this.props.dispatch(deletePostRequest(post));
+    }
+  };
+
+  handleAddPost = (name, title, content, votes) => {
+    this.props.dispatch(toggleAddPost());
+    this.props.dispatch(addPostRequest({ name, title, content, votes }));
+  };
+
+  handleThumbUp = post => {
+    this.props.dispatch(thumbUpPostRequest(post));
+  }
+
+  render() {
+    return (
+      <div>
+        <PostCreateWidget addPost={this.handleAddPost} showAddPost={this.props.showAddPost} />
+        <PostList thumbUp={this.handleThumbUp} handleDeletePost={this.handleDeletePost} posts={this.props.posts} />
+      </div>
+    );
+  }
+}
+
+// Actions required to provide data for this component to render in sever side.
+PostListPage.need = [() => { return fetchPosts(); }];
+
+// Retrieve data from store as props
+function mapStateToProps(state) {
+  return {
+    showAddPost: getShowAddPost(state),
+    posts: getPosts(state),
+  };
+}
+
+PostListPage.propTypes = {
+  posts: PropTypes.arrayOf(PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    content: PropTypes.string.isRequired,
+    votes: PropTypes.number.isRequired
+  })).isRequired,
+  showAddPost: PropTypes.bool.isRequired,
+  dispatch: PropTypes.func.isRequired,
+};
+
+PostListPage.contextTypes = {
+  router: React.PropTypes.object,
+};
+
+export default connect(mapStateToProps)(PostListPage);
